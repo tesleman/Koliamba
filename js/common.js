43a8de41af9b0f48e0772a6a0021fd35577323d3
tesleman <54055976+tesleman@users.noreply.github.com>
2019-12-04 18:39:05 +0200

    1
    1

--- js/common.js
+++ js/common.js
@@ -7,5 +7,31 @@ $(function() {
 		}, 1000);
 	});
 	// Custom JS
+$(".s-adv").waypoint(function() {
 
+	$({blurRadius: 5}).animate({blurRadius: 0}, {
+		duration: 1000,
+		easing: 'swing',
+		step: function() {
+			$(".s-adv-item h3 span").css({
+				"-webkit-filter": "blur("+this.blurRadius+"px)",
+				"filter": "blur("+this.blurRadius+"px)"
+			});
+		}
+	});
+	var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+	$(".s-adv-item h3 span").each(function() {
+		var tcount = $(this).data("count");
+		$(this).animateNumber({ number: tcount,
+			easing: 'easeInQuad',
+			"font-size": "40px",
+			numberStep: comma_separator_number_step},
+			1000);
+	});
+}, {
+  offset: '90%'
+});
+	
+	//Animation-number
+	
 });
