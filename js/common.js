24e6a5988dac2df74a9ba788cef3c5bdec86b29c
tesleman <54055976+tesleman@users.noreply.github.com>
2019-12-12 20:33:54 +0200

    1
    1

--- js/common.js
+++ js/common.js
@@ -53,5 +53,33 @@ $(".portfolio-item").magnificPopup({
 
 });
 
+$(".partner-slider").owlCarousel({
+	dots: false,
+	nav: false,
+	loop: true,
+	autoplay:true,
+	autoplayTimeout:4000,
+	autoplaySpeed: true,
+	autoplaySpeed: 800,
+	autoplayHoverPause:true,
+	smartSpeed: 700,
+	navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+	responsiveClass: true,
+	responsive:{
+		0:{
+			items: 1
+		},
+		800:{
+			items: 2
+		},
+		1100:{
+			items: 3
+		}
+
+	}
+
+
+});
+
 
 });
